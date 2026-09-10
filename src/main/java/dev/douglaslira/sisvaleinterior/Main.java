bd8150b17868d1deacee36b684e739750e483e84
Douglas Lira <douglasliradafonseca.ds@gmail.com>
2026-09-10 15:17:56 -0300

    feat: adiciona classe Main esqueleto

---
+++ src/main/java/dev/douglaslira/sisvaleinterior/Main.java
@@ -0,0 +1,7 @@
+package dev.douglaslira.sisvaleinterior;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("SisVale Interior - Sistema de Controle de Vale Transporte do Interior");
+    }
+}
